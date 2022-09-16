1ab3b377023f796fa751b9270c87ee794f12d1bd
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-09-16 11:53:36 -0300

    update

--- game.js
+++ game.js
@@ -7,7 +7,6 @@
 
 //lista com os invasores        
 let invasores= document.getElementsByClassName('invasor')
-console.log(invasores)
 
 
 // ________________________________________________ 
@@ -40,9 +39,7 @@ let larguraQuadro = document.getElementById('quadro').offsetWidth
 
 
 // ________________________________________________
-// declaração global para as variáveis posX e posY (posicionamentos)
-let posX 
-let posY 
+ 
 
 /*  :?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?*/
 
@@ -50,24 +47,56 @@ let posY
 //recebe parâmetro el que informa o elemento 
 
 const posicElement = (el) => {
-    posX = Math.floor(Math.random()*100)
-    alert(posX)
+    posX = Math.floor(Math.random()*1000)    //Para sorter de 100 para 1000 | posX = Math.floor(Math.random()*900+100) |
+    posY = Math.floor(Math.random()*400) 
+    el.style.position = 'absolute'
+    el.style.left = -posX+'px'
+    el.style.top  = posY+'px'
+}
+
+
+//Desloca os elementos na tela
+//recebe parâmetros elemento, velocidade , incremento
+const moveElemento = (el, veloc, inc) => {
+
+                            //excuta a cada x milissegundos 
+                            const anima = setInterval(() => {
+                                veloc = veloc + inc
+                                el.style.left = (veloc + inc) + 'px'
+                                //Verifica se o elemento saiu do quadro
+                                //classe emtela 
+                                //remove a classe e volta para uma posicao initial
+                                if(veloc > larguraQuadro && el.classList.contain('emtela')){
+                                el.classList.remove('emtela') 
+                                    posicElement(el)
+                                }
+                            }, 33);
 }
 
 
 
 
 
-
-
-
-
-
-
-
+/*  :?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?*/
 
 
 
 //                      EVENTOS E EXECUÇÕES AUTOMÁTICAS 
 
-posicElement('text')
+
+for(inv of invasores){
+    posicElement(inv)
+    posicElement(bom1)
+    moveElemento(inv,Math.random()*25,Math.random()*25)
+    moveElemento(document.getElementById('bom1'),5,3)
+}
+
+
+
+// moveElemento(document.getElementById('inv1'),5,3)
+// moveElemento(document.getElementById('inv2'),5,10)
+// moveElemento(document.getElementById('inv3'),5,20)
+// moveElemento(document.getElementById('bom1'),5,3)
+// posicElement(document.getElementById('inv1'))
+// posicElement(document.getElementById('inv2'))
+// posicElement(document.getElementById('inv3'))
