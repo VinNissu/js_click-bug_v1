60f7f8787965afa5cb88a6bb97d91e9b7789f07b
VinnyAlmeidaBR <110014684+VinnyAlmeidaBR@users.noreply.github.com>
2022-09-19 09:57:44 -0300

    update

--- game.js
+++ game.js
@@ -6,7 +6,7 @@
 
 
 //lista com os invasores        
-let invasores= document.getElementsByClassName('invasor')
+let invasores = document.getElementsByClassName('invasor')
 
 
 // ________________________________________________ 
@@ -17,7 +17,7 @@ let bonzinhos = document.getElementsByClassName('bonzinho')
 
 // ________________________________________________
 //Pontos
-let score = 0 
+let score = 0
 
 
 
@@ -39,7 +39,7 @@ let larguraQuadro = document.getElementById('quadro').offsetWidth
 
 
 // ________________________________________________
- 
+
 
 /*  :?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?:?*/
 
@@ -47,11 +47,11 @@ let larguraQuadro = document.getElementById('quadro').offsetWidth
 //recebe parâmetro el que informa o elemento 
 
 const posicElement = (el) => {
-    posX = Math.floor(Math.random()*1000)    //Para sorter de 100 para 1000 | posX = Math.floor(Math.random()*900+100) |
-    posY = Math.floor(Math.random()*400) 
+    posX = Math.floor(Math.random() * 1000)    //Para sorter de 100 para 1000 | posX = Math.floor(Math.random()*900+100) |
+    posY = Math.floor(Math.random() * 400)
     el.style.position = 'absolute'
-    el.style.left = -posX+'px'
-    el.style.top  = posY+'px'
+    el.style.left = -posX + 'px'
+    el.style.top = posY + 'px'
 }
 
 
@@ -59,21 +59,42 @@ const posicElement = (el) => {
 //recebe parâmetros elemento, velocidade , incremento
 const moveElemento = (el, veloc, inc) => {
 
-                            //excuta a cada x milissegundos 
-                            const anima = setInterval(() => {
-                                veloc = veloc + inc
-                                el.style.left = (veloc + inc) + 'px'
-                                //Verifica se o elemento saiu do quadro
-                                //classe emtela 
-                                //remove a classe e volta para uma posicao initial
-                                if(veloc > larguraQuadro && el.classList.contain('emtela')){
-                                el.classList.remove('emtela') 
-                                    posicElement(el)
-                                }
-                            }, 33);
+    //excuta a cada x milissegundos 
+    const anima = setInterval(() => {
+        veloc = veloc + inc
+        el.style.left = (veloc + inc) + 'px'
+        //Verifica se o elemento saiu do quadro
+        //Ou se foi clicado (classe "morto")
+        //retorna para uma posição
+        //à esquerda quadro (re-entra)  
+        if(veloc > larguraQuadro || el.classList.contains('morto')){
+            //sorteia um valor entre -50 e -500
+            veloc= -Math.random()*450+50
+            inc = Math.random()*5+7
+            posicElement(el)
+            el.classList.remove('morto')
+        }
+        //adiciona atributo velocidade para 
+        //consulta no codigo javascript
+        el.setAttribute('velocidade',inc)   
+    }, 24);
 }
 
 
+
+//AO CLICAR NOS INSETOS 
+
+const clickBug = (el) => {
+    //adiciona a classe morto
+    el.classList.add('morto')
+    //adiciona 10 pontos ao score 
+    score += 10
+    document.getElementById('score').innerText = score
+    //Se a velocidade for maior que 20 faz 100 pontos
+    if(el.getAttribute('velocidade')> 20){
+        score += 100
+    }
+}
 
 
 
@@ -84,12 +105,14 @@ const moveElemento = (el, veloc, inc) => {
 //                      EVENTOS E EXECUÇÕES AUTOMÁTICAS 
 
 
-for(inv of invasores){
+for (inv of invasores) {
     posicElement(inv)
-    posicElement(bom1)
-    moveElemento(inv,Math.random()*25,Math.random()*25)
-    moveElemento(document.getElementById('bom1'),5,3)
+    moveElemento(inv, Math.random() * 1, Math.random() * 5)
+    //evt.target = elemento que executa o evento - inseto clicado
+    inv.addEventListener ('mousedown',(evt)=>{clickBug(evt.target)})
 }
+
+
 
 
 
